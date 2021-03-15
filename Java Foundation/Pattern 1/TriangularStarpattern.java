7ec4f7b27c710a6d09f002bd33dc95e516814143
Aman8991 <52794258+Aman8991@users.noreply.github.com>
2021-03-15 18:44:24 +0530

    Add files via upload

---
+++ Java Foundation/Pattern 1/TriangularStarpattern.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class TriangularStarpattern{
+	public static void main(String[] args){
+		int n = new Scanner(System.in).nextInt();
+		for(int i=1;i<=n;i++){
+			for(int j=0;j<i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
